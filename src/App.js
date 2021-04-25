d2eb2b66c1b5292da25882816dcc4eeaf9a32634
Elizabeth Hahn <elizhahn87@gmail.com>
2021-04-25 15:04:12 -0600

    Add some responsive styling

--- src/App.js
+++ src/App.js
@@ -6,17 +6,17 @@ const App = ()  => {
   return (
     <div className="App">
     <Route exact path="/">
+    <section className="btn-container">
       <header>
         <h1 className="title">HAPPY BIRTHDAY MOM</h1>
       </header>
-      <section className="btn-container">
-        <Link to="/video" className="video-link">
-          <div className="video-link-background">
-            <div className="video-link-background-hover">
-              <p className="video-link-text">CLICK ME</p>
-            </div>
+      <Link to="/video" className="video-link">
+        <div className="video-link-background">
+          <div className="video-link-background-hover">
+            <p className="video-link-text">CLICK ME</p>
           </div>
-        </Link>
+        </div>
+      </Link>
       </section>
       </Route>
       <Route path="/video" component={ Video }/>
